cc1f7d7e48b5f264f98169b27b71ca5acf70fa02
Kateryna Kalantyria <kat.kalantyria@gmail.com>
2018-04-03 20:14:20 +0300

    Lesson2:Task with Strings

--- Test_My_class/src/MyClass.java
+++ Test_My_class/src/MyClass.java
@@ -3,14 +3,12 @@ import java.util.Scanner;
 public class MyClass {
 	public static void main (String[] args) {
        Scanner scanner = new Scanner (System.in);
-       int x = scanner.nextInt();
-       int y = scanner.nextInt();
-       System.out.println(x*y);
-       System.out.println(x-y);
-       System.out.println(x/y);
-       System.out.println(x+y);
+       String S1 = scanner.nextLine();
+       String S2 = scanner.nextLine();
+       String S3 = scanner.nextLine(); 
        
-       
-       
+       System.out.println(S1+S3);
+       System.out.println(S3+S2+S1);
+       System.out.println(S1+S2+S3);
 	}
 }
