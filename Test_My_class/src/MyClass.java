03abe2a9df0f7cfa03aa0c61110024fd666c5170
Kateryna Kalantyria <kat.kalantyria@gmail.com>
2018-04-03 19:24:27 +0300

    Lesson2: Task with numbers and operators

---
+++ Test_My_class/src/MyClass.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MyClass {
+	public static void main (String[] args) {
+       Scanner scanner = new Scanner (System.in);
+       int x = scanner.nextInt();
+       int y = scanner.nextInt();
+       System.out.println(x*y);
+       System.out.println(x-y);
+       System.out.println(x/y);
+       System.out.println(x+y);
+       
+       
+       
+	}
+}
