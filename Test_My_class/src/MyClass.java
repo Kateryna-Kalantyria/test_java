1fb17adc8503d996dd8ebfa23fbd759693eec74a
Kateryna Kalantyria <kat.kalantyria@gmail.com>
2018-04-04 18:12:56 +0300

    Lesson3_task1

--- Test_My_class/src/MyClass.java
+++ Test_My_class/src/MyClass.java
@@ -1,14 +1,18 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class MyClass {
 	public static void main (String[] args) {
-       Scanner scanner = new Scanner (System.in);
-       String S1 = scanner.nextLine();
-       String S2 = scanner.nextLine();
-       String S3 = scanner.nextLine(); 
-       
-       System.out.println(S1+S3);
-       System.out.println(S3+S2+S1);
-       System.out.println(S1+S2+S3);
+     int[]aro = {4,1,2,9,48888,678,2345678,7,8,9};
+     
+     Arrays.sort(aro);
+     
+     System.out.println(Arrays.toString(aro)) ;
+     
+   
+     
+     
+     
+     
+		}
+	
 	}
-}
