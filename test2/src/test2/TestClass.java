d9e5dd6e4e14e8333779823eb0ad828ab4feff0d
Kateryna Kalantyria <kat.kalantyria@gmail.com>
2018-04-16 13:22:22 +0300

    Lesson4Task2 git commit --m Lesson4Task2 git status git commit --m Lesson4Task2

--- test2/src/test2/TestClass.java
+++ test2/src/test2/TestClass.java
@@ -2,28 +2,12 @@ package test2;
 
 public class TestClass {
 	public static void main(String[] args) {
-		
-		for (int i = 10; i <= 20; i++) {
-			System.out.println(i);
-		}
-		System.out.println();
-		int i =10;
-		while (i <= 20) {
-			System.out.println(i++);
-		}
-		System.out.println();
-		i = 10;
-		do {
-			System.out.println(i++);
-		}while (i<=20);
-		    System.out.println();
-		int[] arr = {10,11,12,13,14,15,16,17,18,19,20};
-		for (int x : arr)
-			System.out.println(x);
-			
-			
-		
-		
-	}
 
+		for (int i = 1; i <= 100; i++) {
+			if (i%3 == 0)
+				System.out.println(i);
+
+		}
+
+	}
 }
