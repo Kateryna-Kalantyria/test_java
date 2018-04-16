421060f1ed1381b812b6d90bb95b6ffd1b0af55d
Kateryna Kalantyria <kat.kalantyria@gmail.com>
2018-04-16 15:46:51 +0300

    Lesson4 Task4

--- test2/src/test2/TestClass.java
+++ test2/src/test2/TestClass.java
@@ -1,14 +1,23 @@
 package test2;
 
+import java.util.Random;
+
 public class TestClass {
 	public static void main(String[] args) {
 
-		int[] a = new int[101];
-		for (int i = 0; i <= 101; i++) {
-			a[i] = 101 - i;
-			System.out.println(i);
+		int[] a = new int[10];
+		Random r = new Random();
 
+		for (int i = 0; i < a.length; i++)
+			a[i] = r.nextInt();
+
+		int sum = 0;
+		for (int i = 1; i < a.length - 1; i++) {
+			sum += a[i];
 		}
 
+		System.out.println("Сумма всех элементов массива, кроме первого и последнего  = " + sum);
+
 	}
+
 }
