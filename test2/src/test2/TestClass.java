e1f546c4ac75336f51327697921da5dfa8dc072e
Kateryna Kalantyria <kat.kalantyria@gmail.com>
2018-04-16 11:14:58 +0300

    Lesson4Task

---
+++ test2/src/test2/TestClass.java
@@ -0,0 +1,19 @@
+package test2;
+
+public class TestClass {
+	public static void main(String[] args) {
+
+		int[] x = new int[28];
+
+		for (int i = 0; i < x.length; i++) {
+			x[i] = i * 635;
+			
+			System.out.println(x[0]);
+			System.out.println(x[14]);
+			System.out.println(x[27]);
+			
+		}
+
+	}
+
+}
